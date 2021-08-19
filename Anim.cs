2cabe93db96b4aca915cfa22ecdca13362962d31
EpicSpaces <59315334+EpicSpaces@users.noreply.github.com>
2021-08-19 13:37:42 +0100

    Update Anim.cs

--- Anim.cs
+++ Anim.cs
@@ -6,9 +6,9 @@ using System.Collections.Generic;
 public class Anim : Spatial
 {
 	[Export]
-	public bool start = true;
+	public bool start = false;
 	[Export]
-	public bool gettingpos = true;
+	public bool gettingpos = false;
 
 	[Export]
 	NodePath skeleton_path;
@@ -34,6 +34,7 @@ public class Anim : Spatial
 		// Set all of the bone IDs in bone_IDs, if they are not already made
 		if(gettingpos)
 		{
+			gettingpos = false;
 			for (int j = 0; j < skeleton.GetBoneCount(); j++)
 			{
 				bone_IDs[skeleton.GetBoneName(j)] = skeleton.FindBone(skeleton.GetBoneName(j));
@@ -58,7 +59,7 @@ public class Anim : Spatial
 
 				//	GD.Print(bone_name);
 				//	GD.Print(bone_name_last);
-				if (!skeleton.HasNode(bone_name_last))
+				if (!HasNode(bone_name_last))
 				{
 					Spatial new_node = new Spatial();
 					bone_nodes[j] = new_node;
@@ -66,12 +67,12 @@ public class Anim : Spatial
 
 					if (bone_name.Equals(""))
 					{
-						skeleton.AddChild(bone_nodes[j]);
+						AddChild(bone_nodes[j]);
 					}
 					else
 					{
 						//		GD.Print(bone_name);
-						skeleton.GetNode(bone_name).AddChild(bone_nodes[j]);
+						GetNode(bone_name).AddChild(bone_nodes[j]);
 					}
 					bone_nodes[j].Owner = Owner;
 				}
@@ -81,41 +82,33 @@ public class Anim : Spatial
 					if (bone_name.Equals(""))
 						bone_name = skeleton.GetBoneName(j);
 					
-					bone_nodes[j] = skeleton.GetNode(bone_name) as Spatial;
+				//	bone_nodes[j] = GetNode(bone_name) as Spatial;
 				}
-				// Set the bone node to the currect bone position
-				bone_nodes[j].GlobalTransform = get_bone_transform(j);
+				Transform tsk = bone_nodes[j].GlobalTransform;
+				tsk = get_bone_transform(j);
+		//		tsk.basis.Scale = bone_nodes[j].GlobalTransform.basis.Scale;
+				bone_nodes[j].GlobalTransform = tsk;		
 			}
 		}
 		if(start) 
 		{
-			// Solve the bone chain
 			solve_chain();
 		}
 	}
 	void solve_chain()
 	{
 		chain_apply_rotation();
-		// Reset the bone node transforms to the skeleton bone transforms
-		for (int i = 0; i < bone_nodes.Count; i++)
-		{
-			Transform reset_bone_trans = get_bone_transform(i);
-			bone_nodes[i].Transform = reset_bone_trans;
-		}
 	}
-	// Backward reaching pass
-	// Make all of the bones rotated correctly.
 	void chain_apply_rotation()
 	{
 		for (int i = 0; i < skeleton.GetBoneCount(); i++)
 		{
-			Transform bone_trans = get_bone_transform(i, false);
-			Transform b_target = bone_nodes[i].Transform;
-			//	b_target.origin = skeleton.GlobalTransform.XformInv(b_target.origin);
+			Transform b_target = bone_nodes[i].GlobalTransform;
+			Transform skel = skeleton.GlobalTransform;
+			
+			Transform t=skel.AffineInverse() * b_target;
 
-			bone_trans.basis = bone_nodes[i].Transform.basis;
-			bone_trans.origin = b_target.origin;
-			skeleton.SetBoneGlobalPoseOverride(bone_IDs[skeleton.GetBoneName(i)], bone_trans, 1.0f, true);
+			skeleton.SetBoneGlobalPoseOverride(bone_IDs[skeleton.GetBoneName(i)], t, 1.0f, true);
 		}
 	}
 
@@ -125,7 +118,7 @@ public class Anim : Spatial
 
 		if (convert_to_world_space)
 		{
-		//	ret.origin = skeleton.GlobalTransform.Xform(ret.origin);
+			ret = skeleton.GlobalTransform * ret;
 		}
 		return ret;
 	}
